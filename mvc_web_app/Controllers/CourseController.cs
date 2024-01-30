7d67446d4541ebecc45f964423af1e58b6cbad5f
beyza nur turan <beyzanurturan48@gmail.com>
2024-01-30 21:12:39 +0300

    The controllers and views for the course selection page and applications page have been written.

--- mvc_web_app/Controllers/CourseController.cs
+++ mvc_web_app/Controllers/CourseController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using MVC_WEB_APP.Models;
 
 namespace MVC_WEB_APP.Controllers
 {
@@ -6,11 +7,29 @@ namespace MVC_WEB_APP.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var model =Repository.Applications;
+            return View(model);
         }
         public IActionResult Apply()
         {
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]//sahtecilik vb bir durumda koruma sağlayan bir özelliktir
+        public IActionResult Apply([FromForm] Candidate model) //fromform ile formdan veri geldiğini söyledik. bu şekilde daha sağlıklı 
+
+        {
+            if(Repository.Applications.Any(c=>c.Email.Equals(model.Email)))//modeldeki email ile girilen e mail aynı mı sorgusu
+            {
+                ModelState.AddModelError("","There is already an application for you");
+            }
+            if(ModelState.IsValid)
+            {
+                Repository.Add(model);//formdan gelen bilgileri repository modelinde saklıycak
+            return View("Feedback",model);
+            }
+            return View();
+            
         }
     }
 }
--- mvc_web_app/Models/Candidate.cs
+++ mvc_web_app/Models/Candidate.cs
@@ -1,15 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MVC_WEB_APP.Models
 {
     public class Candidate
     {
+        [Required(ErrorMessage ="E-Mail is required ")]
         public String? Email {get;set;}=string.Empty; //boş bir empty oluşturuyor
+        [Required(ErrorMessage ="FirstName is required ")]
         public String? FirstName {get;set;}=string.Empty;
+        [Required(ErrorMessage ="LastName is required ")]
         public String? LastName {get;set;}=string.Empty;
+        public String? FullName=>$"{FirstName} {LastName?.ToUpper()}";
         public int? Age {get;set;}
         public String? SelectedCourse {get;set;}=string.Empty;
-        public DateTime Apply {get;set;}
+        public DateTime ApplyAt {get;set;}
         public Candidate(){//her nesne üretildiğinde tarih bilgisinin oluşmasını sağlar
-            Apply=DateTime.Now;
+            ApplyAt=DateTime.Now;
         }
     } 
         
