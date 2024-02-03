1e88f3b367f07d0da33af6180b689b8d59516942
beyza nur turan <beyzanurturan48@gmail.com>
2024-02-03 21:30:33 +0300

    Operations for the admin have been initiated.

--- mvc_web_app/Store/StoreApp/Controllers/ProductController.cs
+++ mvc_web_app/Store/StoreApp/Controllers/ProductController.cs
@@ -4,28 +4,31 @@ using Microsoft.EntityFrameworkCore;
 using Repositories;
 using Entities.Models;
 using Repositories.Contracts;
+using Services.Contracts;
 
 namespace StoreApp.Controllers
  {
     public class ProductController:Controller
     {
-        private readonly IRepositoryManager _manager;
+        private readonly IServiceManager _manager;
 
-        //dependency injection patterni kullanılarak bilgi alma kullanma
-        public ProductController(IRepositoryManager manager)
-        { 
+        public ProductController(IServiceManager manager)
+        {
             _manager = manager;
         }
+
+        //dependency injection patterni kullanılarak bilgi alma kullanma
+
 
         public IActionResult Index() //tüm ürünleri listeler
         {
             
-            var model = _manager.Product.GetAllProducts(false).ToList();
+            var model = _manager.ProductService.GetAllProducts(false).ToList();
             return View(model);
         }
-        public IActionResult Get(int id) //id ye göre ürün getirir
+        public IActionResult Get([FromRoute(Name ="id")]int id) //id ye göre ürün getirir
         {
-            var model= _manager.Product.GetOneProduct(id,false);
+            var model= _manager.ProductService.GetOneProduct(id,false);
             return View(model);
         }
 
--- mvc_web_app/Store/StoreApp/Program.cs
+++ mvc_web_app/Store/StoreApp/Program.cs
@@ -1,21 +1,31 @@
 using Microsoft.EntityFrameworkCore;
 using Repositories;
 using Repositories.Contracts;
+using Services;
+using Services.Contracts;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<RepositoryContext>(options=>
+builder.Services.AddDbContext<RepositoryContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"),
-    b=>b.MigrationsAssembly("StoreApp"));
+    b => b.MigrationsAssembly("StoreApp"));
 });
-builder.Services.AddScoped<IRepositoryManager,RepositoryManager>();
-builder.Services.AddScoped<IProductRepository,ProductRepository>();
-builder.Services.AddScoped<ICategoryRepository,CategoryRepository>();
+builder.Services.AddScoped<IRepositoryManager, RepositoryManager>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IServiceManager, ServiceManager>();
+builder.Services.AddScoped<IProductService, ProductManager>();
+builder.Services.AddScoped<ICategoryService, CategoryManager>();
 var app = builder.Build();
 app.UseStaticFiles();//wwwroot klasörünü kullanılabilir hale getirdik
 app.UseHttpsRedirection();
 app.UseRouting();
-app.MapControllerRoute("default","{controller=Home}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+{   
+    endpoints.MapAreaControllerRoute("Admin","Admin","Admin/{controller=Dashboard}/{action=Index}/{id?}");
+    endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+});
+
 
 app.Run();
