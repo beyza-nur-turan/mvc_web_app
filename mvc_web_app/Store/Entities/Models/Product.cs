7a39cae9570835144fc9780f3e0b97574ca49370
beyza nur turan <beyzanurturan48@gmail.com>
2024-02-04 21:50:33 +0300

    Necessary additions were made

--- mvc_web_app/Store/Entities/Models/Product.cs
+++ mvc_web_app/Store/Entities/Models/Product.cs
@@ -1,7 +1,13 @@
-﻿namespace Entities.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Models;
 public class Product
 {
     public int productId { get; set; }
+    [Required(ErrorMessage ="ProductName is required")]
     public String productName { get; set; } = String.Empty;
+    [Required(ErrorMessage ="Price is required")]
     public decimal price { get; set; }
+    public int? CategoryId {get;set;}
+    public Category? category {get;set;}
 }
