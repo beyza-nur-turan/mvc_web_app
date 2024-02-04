dc2776a19e54b342e0e8fb8251bd6cd39360b8e1
beyza nur turan <beyzanurturan48@gmail.com>
2024-02-04 21:52:21 +0300

    Service process was written to update the product

--- mvc_web_app/Store/Services/ProductManager.cs
+++ mvc_web_app/Store/Services/ProductManager.cs
@@ -1,4 +1,5 @@
 using Entities.Models;
+using Microsoft.VisualBasic;
 using Repositories.Contracts;
 using Services.Contracts;
 
@@ -11,6 +12,23 @@ namespace Services
         public ProductManager(IRepositoryManager manager)
         {
             _manager = manager;
+        }
+
+        public void CreateProduct(Product product)
+        {
+            _manager.Product.Create(product);
+            _manager.Save();
+        }
+
+        public void DeleteOneProduct(int id)
+        {
+            Product product=GetOneProduct(id,false) ;
+            if(product is not null)
+            {
+                _manager.Product.DeleteOneProduct(product);
+                _manager.Save();
+            }
+            
         }
 
         public IEnumerable<Product> GetAllProducts(bool trackChanges)
@@ -26,6 +44,14 @@ namespace Services
                 throw new Exception("Product not found");
             }
             return product;
+        }
+
+        public void UpdateOneProduct(Product product)
+        {
+            var entity=_manager.Product.GetOneProduct(product.productId,true);
+            entity.productName=product.productName;
+            entity.price=product.price;
+            _manager.Save();
         }
     }
 }
